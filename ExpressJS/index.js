dcb1453ef3d4c656000e629457c20a4a1aec55e4
JianHui1208 <jianhuiwilliam80@gmail.com>
2021-02-08 02:02:39 +0800

    Style and Script

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -15,5 +15,20 @@ app.get('/dynamic_view', function(req, res) {
     });
 });
 
+app.get('/home', function(req, res) {
+    res.render('home', {
+        user: { name: "Ali", age: "20" }
+    });
+});
+
+app.get('/sign_up', function(req, res) {
+    res.render('sign_up');
+});
+
+app.get('/content', function(req, res) {
+    res.render('content');
+});
+
+
 console.log('Server running at http://127.0.0.1:8888/');
 app.listen(8888);
